6233c1987df1db46592eb0fe911271c05de8bf95
NatanaelDp <135743045+NatanaelDp@users.noreply.github.com>
2023-07-25 16:12:56 -0300

    Add files via upload

---
+++ Aula12Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Aula12Classes;
+
+Console.WriteLine("Olá, mundo!");
+
+Personagem guerreiro= new Personagem();
+guerreiro.nome = "Rakan";
+guerreiro.raca = "Vastaya";
+guerreiro.poder = 500;
+
+guerreiro.mostra();
+Personagem mago= new Personagem();
+mago.raca = "Elfo";
+mago.poder = 150;
+mago.nome = "Harry";
+
+mago.mostra();
+
+Personagem bruxo = mago;
+bruxo.nome = "Hermione";
+bruxo.mostra();
+
+Personagem atirador= new Personagem();
+atirador.nome = "Jhin";
+atirador.raca = "Humano";
+atirador.poder = 4444;
+
+atirador.mostra();
+
+
+
+
+
+
+Console.ReadKey();
+
